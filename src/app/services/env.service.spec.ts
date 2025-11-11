2adaca862e3b4daa69154961d23c3ec265d136e4
Ladji CISSE <ladjicisse.pro@gmail.com>
2025-11-11 11:45:37 +0100

    fix karma unit test

--- src/app/services/env.service.spec.ts
+++ src/app/services/env.service.spec.ts
@@ -1,3 +1,5 @@
+import 'zone.js';
+import 'zone.js/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { EnvService } from './env.service';
@@ -6,11 +8,24 @@ describe('EnvService', () => {
   let service: EnvService;
 
   beforeEach(() => {
+
+    // (window as any).__env__ = {
+    //   API_BASE_URL: 'https://mock-api.local',
+    //   DYNATRACE_RUM_URL: 'https://mock-rum.local',
+    //   FEATURE_FLAG_NEW_DASHBOARD: true,
+    // };
+
+    spyOn(document.head, 'appendChild').and.stub();
+
     TestBed.configureTestingModule({});
     service = TestBed.inject(EnvService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should load values from window.__env__', () => {
+    expect(service.getDynamicRumUrl()).toBe('assets/test-rum.js');
   });
 });
