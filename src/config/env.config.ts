4f0926179e6519580f836e0666dc83b115301d32
Ladji CISSE <ladjicisse.pro@gmail.com>
2025-11-12 20:24:09 +0100

    fix locals env file treatment

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,11 +1,11 @@
 import {IEnvConfig} from './env.model';
 
 // Load runtime environment variables
-const runtimeEnv = (window as any).__env__ || {};
 const runtimeLocalEnv = (window as any).__env__local || {};
+const runtimeEnv = (window as any).__env__ || {};
 
 // Destructure and export environment variables
 export const envConfig: IEnvConfig = {
-  ...runtimeEnv,
-  ...runtimeLocalEnv
+  ...runtimeLocalEnv,
+  ...runtimeEnv
 };
