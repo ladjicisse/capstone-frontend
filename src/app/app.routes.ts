c1ce6f3e6675dd2e2d7ddf14e7afeb2876fd91ad
Ladji CISSE <ladjicisse.pro@gmail.com>
2025-11-18 19:50:56 +0100

    do not show input search from header when we on register page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,8 @@ import {anonymousGuard} from './guards/anonymous.guard';
 
 export const routes: Routes = [
   { path: '', loadComponent: () => import('./pages/home/home').then(m => m.Home)},
-  { path: 'register', loadComponent: () => import('./pages/register/register').then(m => m.Register), canActivate: [anonymousGuard]},
   { path: 'products/:id', loadComponent: () => import('./pages/product-detail/product-detail').then(m => m.ProductDetail) },
-  { path: 'profile', loadComponent: () => import('./pages/profile/profile').then(m => m.Profile), canActivate: [MsalGuard] },
-  { path: 'basket', loadComponent: () => import('./pages/basket/basket').then(m => m.Basket) }
+  { path: 'basket', loadComponent: () => import('./pages/basket/basket').then(m => m.Basket) },
+  { path: 'register', loadComponent: () => import('./pages/register/register').then(m => m.Register), canActivate: [anonymousGuard], data: { hideSearch: true }},
+  { path: 'profile', loadComponent: () => import('./pages/profile/profile').then(m => m.Profile), canActivate: [MsalGuard] }
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import {Component, computed, inject, OnInit, signal} from '@angular/core';
-import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {AuthService} from './services/auth.service';
 import {AsyncPipe} from '@angular/common';
 import {BasketStore} from './store/basket.store';
 import {FormsModule} from '@angular/forms';
 import {SearchService} from './services/search.service';
 import {MsalService} from '@azure/msal-angular';
+import {filter} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -19,12 +20,16 @@ export class App implements OnInit {
   private searchService = inject(SearchService);
   protected basketStore = inject(BasketStore);
   private msalService = inject(MsalService);
+  private router = inject(Router);
+  private activatedRoute = inject(ActivatedRoute);
 
   protected searchTerm = computed(() => this.searchService.query());
 
   protected basketItemsCount = computed(() => this.basketStore.itemsCount());
 
   protected readonly currentAccount$ = this.authService.getCurrentAccount$();
+
+  protected hideSearch = false;
 
   // async ngOnInit() {
   //
@@ -50,6 +55,13 @@ export class App implements OnInit {
       },
       error: (error) => console.log(error)
     });
+
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      const child = this.activatedRoute.firstChild;
+      this.hideSearch = child?.snapshot.data['hideSearch'] ?? false;
+    })
   }
 
   onSearch(event: any) {
